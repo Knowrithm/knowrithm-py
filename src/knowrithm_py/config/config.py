dc3b3263745cf4268ad956d2f000128f7738b269
Steven Waheed <steven@aqaryint.com>
2025-09-03 17:07:27 +0400

    adjust sdk connection

--- src/knowrithm_py/config/config.py
+++ src/knowrithm_py/config/config.py
@@ -1,4 +1,5 @@
 
 class Config:
-    KNOWRITHM_BASE_URL = 'https://app.knowrithm.org/api'
+    # KNOWRITHM_BASE_URL = 'https://app.knowrithm.org/api'
+    KNOWRITHM_BASE_URL = 'http://localhost:8543/api'
     
